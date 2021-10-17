6ede7266c18d07d4fa456f2875c32f6aab173c0c
Carlos Hernadnez Perez <carloshe@qrsof.com>
2021-10-16 21:49:15 -0500

    psr4 v1

---
+++ bbva/Data/Bbva.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace BBVA\Data;
+
+class Bbva
+{
+
+    private static $instance = null;
+    private static $id = '';
+    private static $apiKey = '';
+    private static $apiEndpoint = 'https://api.ecommercebbva.com/v1';
+    private static $apiSandboxEndpoint = 'https://sand-api.ecommercebbva.com/v1';
+    private static $sandboxMode = true;
+
+    public function __construct()
+    {
+
+    }
+
+    public static function getInstance($id = '', $apiKey = '')
+    {
+        if ($id != '') {
+            self::setId($id);
+        }
+        if ($apiKey != '') {
+            self::setApiKey($apiKey);
+        }
+        $instance = BbvaApi::getInstance(null);
+        return $instance;
+    }
+
+    public static function setApiKey($key = '')
+    {
+        if ($key != '') {
+            self::$apiKey = $key;
+        }
+    }
+
+    public static function getApiKey()
+    {
+        $key = self::$apiKey;
+        if (!$key) {
+            $key = getenv('API_KEY');
+        }
+        return $key;
+    }
+
+    public static function setId($id = '')
+    {
+        if ($id != '') {
+            self::$id = $id;
+        }
+    }
+
+    public static function getId()
+    {
+        $id = self::$id;
+        if (!$id) {
+            $id = getenv('MERCHANT_ID');
+        }
+        return $id;
+    }
+
+    public static function getSandboxMode()
+    {
+        $sandbox = self::$sandboxMode;
+        if (getenv('PRODUCTION_MODE')) {
+            $sandbox = (strtoupper(getenv('PRODUCTION_MODE')) == 'FALSE');
+        }
+        return $sandbox;
+    }
+
+    public static function setSandboxMode($mode)
+    {
+        self::$sandboxMode = $mode ? true : false;
+    }
+
+    public static function getProductionMode()
+    {
+        $sandbox = self::$sandboxMode;
+        if (getenv('PRODUCTION_MODE')) {
+            $sandbox = (strtoupper(getenv('PRODUCTION_MODE')) == 'FALSE');
+        }
+        return !$sandbox;
+    }
+
+    public static function setProductionMode($mode)
+    {
+        self::$sandboxMode = $mode ? false : true;
+    }
+
+    public static function getEndpointUrl()
+    {
+        return (self::getSandboxMode() ? self::$apiSandboxEndpoint : self::$apiEndpoint);
+    }
+
+}
---
+++ bbva/Data/BbvaApiConnectionError.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace BBVA\Data;
+
+class BbvaApiConnectionError extends BbvaApiError
+{
+
+}
